80b1010585d5045c843168dacd498fcc1fea59c3
Flikimax <flikimax@gmail.com>
2021-07-23 19:57:27 -0500

    Se agrega composer

---
+++ wp-framework.php
@@ -0,0 +1,22 @@
+<?php
+/*
+@wordpress-plugin
+Plugin Name:    Wp Framework 
+Plugin URI:     github.com/Flikimax/Wp-Framework
+Description:    Framework mvc (model-view-controller) desarrollado con la intensión de ayudar y disminuir el tiempo de desarrollo de sistemas.
+Version:        0.1
+Author:         Flikimax     
+Author URI:     flikimax.com
+*/
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+require __DIR__ . '/vendor/autoload.php';
+define('FW_PREFIX', 'fw_');
+
+
+
+
+
